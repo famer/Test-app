26822783ed40425e56857acb68b7cbfc51d2b429
Timur Tatarshaov <tatarshaov@gmail.com>
2015-03-03 17:18:55 +0100

    Initial Commit

---
+++ Test app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test app
+//
+//  Created by Тимур Татаршаов on 03.03.15.
+//  Copyright (c) 2015 Timur Tatarshaov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
